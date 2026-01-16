60412c8d9b04406bc5b65a6dfdbdc172083569ee
charlescordialDevExpress <charles.cordial@devsoft.ph>
2026-01-16 18:06:09 +0800

    Revert "jQuery PR POC"

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,76 +1,10 @@
 $(() => {
-  $('#gridContainer').dxDataGrid({
-    dataSource: customers,
-    keyExpr: 'ID',
-    showBorders: true,
-    filterRow: {
-      visible: true,
+  let count = 0;
+  $('#btn').dxButton({
+    text: `Click count: ${count}`,
+    onClick(e) {
+      count += 1;
+      e.component.option('text', `Click count: ${count}`);
     },
-    onEditorPreparing: (e) => {
-      // Customize boolean filter editor via e.editorName
-      if (e.parentType === 'filterRow' && e.dataField === 'IsActive') {
-        e.editorName = 'dxCheckBox';
-        e.editorOptions = {
-          value: null,
-          enableThreeStateBehavior: true,
-          onValueChanged(args) {
-            e.setValue(args.value ?? null);
-          }
-        };
-      }
-
-      // Customize category filter editor via DOM injection to e.editorElement
-      if (e.parentType === 'filterRow' && e.dataField === 'CategoryId') {
-        e.cancel = true;
-        e.editorElement.append(createCustomEditor(e));
-      }
-    },
-    columns: [
-      {
-        dataField: 'ID',
-        width: 80,
-        visible: false,
-      },
-      {
-        dataField: 'CompanyName',
-        caption: 'Company Name',
-      },
-      {
-        dataField: 'IsActive',
-        caption: 'Active',
-        dataType: 'boolean',
-      },
-      {
-        dataField: 'CategoryId',
-        caption: 'Category',
-        calculateDisplayValue: (row) => {
-          return categories.find(c => c.id === row.CategoryId)?.name;
-        }
-      },
-    ],
   });
-
-  function createCustomEditor(dataGridArgs) {
-    return $('<div>').dxDropDownBox({
-      dataSource: categories,
-      valueExpr: 'id',
-      displayExpr: 'name',
-      contentTemplate: (dropDownArgs) => {
-        return $('<div>').dxTreeList({
-          dataSource: categories,
-          keyExpr: 'id',
-          parentIdExpr: 'parentId',
-          selection: { mode: 'single' },
-          columns: ['name'],
-          onSelectionChanged: (treeListArgs) => {
-            const selectedId = treeListArgs.currentSelectedRowKeys[0];
-            if (!selectedId) return;
-            dataGridArgs.setValue(selectedId);
-            dropDownArgs.component.option('value', selectedId);
-            dropDownArgs.component.close();
-          },
-        });
-      },
-    });
-  }
 });
