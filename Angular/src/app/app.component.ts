cda4e36817d0ca1c4adda0c941bc80b229318cb3
Charles Cordial <charles.cordial@devsoft.ph>
2026-03-27 16:47:21 +0800

    Fix: Explicitly define standAlone

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -14,6 +14,7 @@ import { one } from "devextreme/events";
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: false,
 })
 export class AppComponent {
   @ViewChild("treeViewComponent", { static: false }) treeViewComponent!: DxTreeViewComponent;
